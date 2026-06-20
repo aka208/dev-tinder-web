35148542fc571bee81b14e9c4fdfa4bd27571ca4
Akash Singh <2081993akash@gmail.com>
2026-06-20 23:55:08 +0530

    Data authentication and profile api integrated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,38 @@
 // import React from 'react'
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const userData = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(`${BASE_URL}profile/view`, {
+        withCredentials: true,
+      });
+      const user = res.data;
+      dispatch(addUser(user));
+      console.log(res.data);
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <Navbar />
