7d8b166687b836a56a7a41bd2969d7ecd415df04
Akash Singh <2081993akash@gmail.com>
2026-06-30 23:06:24 +0530

    login api issue fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
   const onLoginClick = async () => {
     try {
       const res = await axios.post(
-        BASE_URL + "login",
+        `${BASE_URL}/login`,
         {
           emailId,
           password,
